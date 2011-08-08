4cd9997a62f28772127d84941a471ff9b806192c
Cameron Manderson <cameronmanderson@gmail.com>
2011-08-08 11:49:31 +1000

    Introduced the Slugger Object Service
    Refactored the object graph to split logic that generates the slug from
    the Listener into an object service named the Slugger. This allows other
    developers to implement their own slug generation and specify using the
    Service XML (DI). This allows authors the flexibility to create slugs
    based on the known fields (e.g. in example; "example-document-by-cam")
    or handle collisions differently (e.g. in example;
    "example-document-2-by-cam"). This also allows for better independent
    testing of the logic around generating of slugs, as you can simply
    create the service, punch in test data (e.g. bad characters etc) and
    test the result.  Updated the documentation, introduced a
    SluggerInterface and implementation of Slugger (based on the slugify
    doctrine method). Removed the static implementation of the collection of
    collisions and passed through to the slugger.

--- src/SamJ/DoctrineSluggableBundle/Listener/SluggableListener.php
+++ src/SamJ/DoctrineSluggableBundle/Listener/SluggableListener.php
@@ -3,14 +3,26 @@
 namespace SamJ\DoctrineSluggableBundle\Listener;
 
 use SamJ\DoctrineSluggableBundle\SluggableInterface;
+use SamJ\DoctrineSluggableBundle\SluggerInterface;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 
+/**
+ *
+ * @TODO: Eliminate dependency on 'getId' from the entity from the interface
+ */
 class SluggableListener 
 {
-	protected static $issuedSlugs = array();
+    protected $slugger;
+
+    /**
+     * @param Slugger $slugger
+     */
+    public function __construct(SluggerInterface $slugger) {
+        $this->slugger = $slugger;
+    }
 
 	public function prePersist(LifecycleEventArgs $ea)
 	{
@@ -28,36 +40,36 @@ class SluggableListener
 		return $em->getRepository(get_class($entity));
 	}
 
+    /**
+     * @param \SamJ\DoctrineSluggableBundle\SluggableInterface $entity
+     * @param \Doctrine\ORM\EntityRepository $repository
+     * @return void
+     *
+     * @TODO: Remove the dependency on the field 'slug' on the entity (not in interface)
+     * @TODO: Discuss whether the slug should auto-update if it is an 'update' (behaviour?)
+     */
 	protected function generateUniqueSlug(SluggableInterface $entity, EntityRepository $repository)
 	{
-		if (!isset(self::$issuedSlugs[get_class($entity)])) self::$issuedSlugs[get_class($entity)] = array();
+		// Find a slug
+        $eliminated = array(); // Our prior eliminated slugs
+        $foundSlug = false;
+		do {
+            // Obtain our slug
+            $slug = $this->slugger->getSlug($entity->getSlugFields(), $eliminated);
 
-		$slug = is_array($entity->getSlugFields()) ? implode('-', $entity->getSlugFields()) : $entity->getSlugFields();
-		// replace non letter or digits by -
-		$slug = preg_replace('~[^\\pL\d]+~u', '-', $slug);
-		// trim
-		$slug = trim($slug, '-');
-		// transliterate
-		if(function_exists('iconv')):
-				$slug = iconv('utf-8', 'us-ascii//TRANSLIT', $slug);
-		endif;
-		// lowercase
-		$slug = strtolower($slug);
-		// remove unwanted characters
-		$slug = preg_replace('~[^-\w]+~', '', $slug);
-		$loops = 0;
-		do
-		{
-			++$loops;
-			$testSlug = $slug;
-			if ($loops > 1) $testSlug .= '-' . $loops;
+            // See if it is in our collection
+            $result = $repository->findOneBy(array('slug' => $slug));
 
-			$result = $repository->findOneBy(array('slug' => $testSlug));
-		} while ((!empty($result) && $result->getId() != $entity->getId()) ||
-		         in_array($testSlug, self::$issuedSlugs[get_class($entity)]));
+            // Check to see if we have found a slug that matches
+            if(!empty($result) && $result->getId() != $entity->getId()) {
+                $eliminated[] = $slug;
+            } else {
+                // We have found a slug for this element
+                $foundSlug = true;
+            }
+		} while ($foundSlug === false);
 
-		$entity->setSlug($testSlug);
-		
-		self::$issuedSlugs[get_class($entity)][] = $testSlug;
+        // Set the slug back to the entity
+		$entity->setSlug($slug);
 	}
 }
