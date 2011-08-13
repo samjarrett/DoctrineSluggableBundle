f806b672afb7bd96769e147eaf7d0dc4b9486982
Cameron Manderson <cameronmanderson@gmail.com>
2011-08-14 08:15:33 +1000

    Added transliterate test to Slugger

--- tests/DoctrineSluggable/Test/DefaultSluggerTest.php
+++ tests/DoctrineSluggable/Test/DefaultSluggerTest.php
@@ -9,17 +9,24 @@ class DefaultSluggerTest extends \PHPUnit_Framework_TestCase
     public function testGetSlug()
     {
         $slugger = new Slugger();
-        $this->assertEquals($slugger->getSlug('An Example'), 'an-example');
-        $this->assertEquals($slugger->getSlug('AnExample'), 'anexample');
-        $this->assertEquals($slugger->getSlug('The "Fancy-Example"'), 'the-fancy-example');
+        $this->assertEquals($slugger->getSlug('An Example'), 'an-example', 'Simple behaviour to replace whitespace and lowercase');
+        $this->assertEquals($slugger->getSlug('AnExample'), 'anexample', 'Case insensitivity');
+        $this->assertEquals($slugger->getSlug('The "Fancy-Example"'), 'the-fancy-example', 'First set of special characters');
     }
 
     public function testGetSlugExcludeList()
     {
         $slugger = new Slugger();
-        $this->assertEquals($slugger->getSlug('An Example', array('an-example')), 'an-example-1');
-        $this->assertEquals($slugger->getSlug('An Example', array('an-example', 'an-example-1')), 'an-example-2');
-        $this->assertEquals($slugger->getSlug('An Example', array('an-example', 'an-example-2')), 'an-example-1');
-        $this->assertEquals($slugger->getSlug('An Example', array('an-example', 'an-example-1', 'an-example-3')), 'an-example-2');
+        $this->assertEquals($slugger->getSlug('An Example', array('an-example')), 'an-example-1', 'Create unique if appears in exclude list');
+        $this->assertEquals($slugger->getSlug('An Example', array('an-example', 'an-example-1')), 'an-example-2', 'Ensure correct sequencing if exclude list includes first unique attempt');
+        $this->assertEquals($slugger->getSlug('An Example', array('an-example', 'an-example-2')), 'an-example-1', 'Find the first unique e.g. -1 if available');
+        $this->assertEquals($slugger->getSlug('An Example', array('an-example', 'an-example-1', 'an-example-3')), 'an-example-2', 'Find the first unique if -1 is available and -3 is taken');
+    }
+
+    public function testGetSlugIconV()
+    {
+        $slugger = new Slugger();
+        $this->assertEquals($slugger->getSlug('An éxample'), 'an-example', 'Transliterate é to e');
+        // @TODO: Create more examples of transliterate
     }
 }
