84953dff7049c8b530d9532a286f46e691269ddb
Yvan <yvmarques@gmail.com>
2014-03-20 16:55:41 +0100

    Fix test fails
    Slugger is waiting for two arguments that are missing when testing.

--- tests/DoctrineSluggable/Test/DefaultSluggerTest.php
+++ tests/DoctrineSluggable/Test/DefaultSluggerTest.php
@@ -8,7 +8,7 @@ class DefaultSluggerTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetSlug()
     {
-        $slugger = new Slugger();
+        $slugger = new Slugger('-', '-');
         $this->assertEquals($slugger->getSlug('An Example'), 'an-example', 'Simple behaviour to replace whitespace and lowercase');
         $this->assertEquals($slugger->getSlug('AnExample'), 'anexample', 'Case insensitivity');
         $this->assertEquals($slugger->getSlug('The "Fancy-Example"'), 'the-fancy-example', 'First set of special characters');
@@ -17,7 +17,7 @@ class DefaultSluggerTest extends \PHPUnit_Framework_TestCase
 
     public function testGetSlugExcludeList()
     {
-        $slugger = new Slugger();
+        $slugger = new Slugger('-', '-');
         $this->assertEquals($slugger->getSlug('An Example', array('an-example')), 'an-example-1', 'Create unique if appears in exclude list');
         $this->assertEquals($slugger->getSlug('An Example', array('an-example', 'an-example-1')), 'an-example-2', 'Ensure correct sequencing if exclude list includes first unique attempt');
         $this->assertEquals($slugger->getSlug('An Example', array('an-example', 'an-example-2')), 'an-example-1', 'Find the first unique e.g. -1 if available');
@@ -26,7 +26,7 @@ class DefaultSluggerTest extends \PHPUnit_Framework_TestCase
 
     public function testGetSlugIconV()
     {
-        $slugger = new Slugger();
+        $slugger = new Slugger('-', '-');
         $this->assertEquals($slugger->getSlug('An éxample'), 'an-example', 'Transliterate é to e');
         // @TODO: Create more examples of transliterate
     }
